725fc2ffd0e576bbb03722b6a0210f506066f0a0
Juha Heimonen <heimojuh@gmail.com>
2014-10-05 22:43:51 +0300

    Adds documentationish string to create

--- src/condensator/core.clj
+++ src/condensator/core.clj
@@ -2,4 +2,6 @@
   (:require [clojurewerkz.meltdown.reactor :as mr]))
 
 (defn create []
+  "Creates the container object for consumers. In the future
+  also the networking functionality is in the returned object"
   (mr/create))
