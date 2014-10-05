c67d65e5ec144707cee5191f7338eaf325fa63c8
Juha Heimonen <heimojuh@gmail.com>
2014-10-05 17:00:44 +0300

    Adds dependencies for reactor and meltdown an timbre

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject condensator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "condensator clojure library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojurewerkz/meltdown "1.0.0"]
+                 [com.taoensso/timbre "3.2.1"]
+                 [org.projectreactor/reactor-net "1.1.4.RELEASE"] 
+                 [org.projectreactor/reactor-core "1.1.4.RELEASE"] ])
