70e41237874b1cc4afab9f289a5ba875d33bf4f9
Juha Heimonen <heimojuh@gmail.com>
2014-10-09 10:41:45 +0300

    Adds test for ON method and plain reactor

--- src/condensator/core.clj
+++ src/condensator/core.clj
@@ -1,7 +1,16 @@
 (ns condensator.core
-  (:require [clojurewerkz.meltdown.reactor :as mr]))
+  (:require [clojurewerkz.meltdown.reactor :as mr]
+            [clojurewerkz.meltdown.selectors :refer  [$]]))
 
 (defn create []
   "Creates the container object for consumers. In the future
   also the networking functionality is in the returned object"
   (mr/create))
+
+(defn notify [condensator selector payload]
+  "Notifies condensator with payload based on selector")
+
+(defn on [condensator selector fn]
+  "Attaches listener to condensator"
+  (mr/on condensator ($ selector ) fn))
+
--- test/condensator/core_test.clj
+++ test/condensator/core_test.clj
@@ -1,9 +1,28 @@
 (ns condensator.core-test
   (:use [speclj.core])
-  (:require [condensator.core :as condensator]))
+  (:require [condensator.core :as condensator]
+            [clojurewerkz.meltdown.selectors :refer  [$]]
+            [taoensso.timbre :as timbre]))
+
+
+(timbre/refer-timbre)
 
 (describe "Create tests"
           (it "Creates instance of Reactorish object" 
               (should= (type (condensator/create)) reactor.core.Reactor)))
 
-(run-specs)
+(defn get-registry-from-reactor [r] 
+  (seq (.getConsumerRegistry r)))
+
+(defn get-registry-entry-by-selector [registry selector]
+    (filter #(= (.getObject (.getSelector %)) selector) registry))
+
+(describe "On tests"
+          (it "Attaches listener to Reactorish object"
+              (let [c (condensator/create)]
+                (condensator/on c "fookey" (fn [] ""))
+                (let [registry (get-registry-from-reactor c)]
+                  ;Counts 2 because first one is always Reactor instance
+                  (should= 2 (count registry))
+                  (should= 1 (count (get-registry-entry-by-selector registry "fookey")))))))
+
